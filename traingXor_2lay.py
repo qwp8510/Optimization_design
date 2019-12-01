15efc6382aff5557789099ffdc782777f6077122
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2019-12-01 13:29:06 +0800

    testing predict report

--- traingXor_2lay.py
+++ traingXor_2lay.py
@@ -15,23 +15,28 @@ def neural_learning(x):
     z_1, z_2 = _activation(w11*x_1 + w12*x_2 + b1), _activation(w21*x_1 + w22*x_2 + b2)
     err = y_d - _activation(w31*z_1 + w32*z_2 + b3)
     err = np.linalg.norm(err)
-    print('err',err)
+    print('err:',err)
     return err
 
 def neural_predict(x,in1,in2):
     w11, w12, w21, w22, w31, w32 =  x[0], x[1], x[3], x[4], x[6], x[7]
     b1,b2,b3 = x[2], x[5], x[8]
-    x_1 = in1
-    x_2 = in2
+    x_1 = np.array(in1)
+    x_2 = np.array(in2)
     z_1, z_2 = _activation(w11*x_1 + w12*x_2 + b1), _activation(w21*x_1 + w22*x_2 + b2)
     f = _activation(w31*z_1 + w32*z_2 + b3)
+    print('predict:',f)
+    return f
     
 if __name__ == '__main__':
     x0 = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-    xpot = CGradDecent(neural_learning, x0, 9, Gradient='Central', LineSearch = 'FiS', MinNorm = 0.001, MaxIter = 300000).RunOptimization()
+    #xpot = CGradDecent(neural_learning, x0, 9, Gradient='Central', LineSearch = 'FiS', MinNorm = 0.001, MaxIter = 300000).RunOptimization()
+    xpot = [6.2763870796984005, 6.276624154295818, -9.586595138099005, 8.009982570820826, 8.01090375646171, 
+-3.7173554243791616, -16.022370279421505, 15.428295873785036, -7.418001687879613]
     neural_learning(x0)
-    print(xpot)
     neural_learning(xpot)
+    neural_predict(x0,[0,0,1,1],[0,1,0,1])
+    neural_predict(xpot,[0,0,1,1],[0,1,0,1])
 
 """
 
@@ -58,6 +63,9 @@ err 0.0015182213926920065
 7(central,MinNorm = 0.001,FiS):[6.276385007031306, 6.276622083283425, -9.586592033915844, 8.00997999000639, 8.010901181592404, 
 -3.717354091844273, -16.022363939519728, 15.428289419644107, -7.417998377559355]
 err 0.001518226924882754
+
+8 preidct: 原:err: 1.2449576748954105,predict: [0.86040223 0.87390429 0.87033548 0.88142759]
+           新:err: 0.0015182213926920065,predict: [8.63963842e-04 9.99281739e-01 9.99281696e-01 7.25742659e-04]
  """
 
 
