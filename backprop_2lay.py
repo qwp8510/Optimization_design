42a745f9b9a435b15c0e1273c6db8830bf27cb62
姜韋辰 <qwp8510@pc215.wireless251.tku.edu.tw>
2019-12-12 14:08:07 +0800

    fixing forword for more reasonable base on nuro==weight

--- backprop_2lay.py
+++ backprop_2lay.py
@@ -16,14 +16,16 @@ class Neuron():
         temp_err = 0
         inputs = np.array(inputs)
         x_1, x_2, y_d = inputs[0], inputs[1], inputs[2]
-        for i in range(0, len(inputs)-1, 1):
+        for i in range(len())
+
+        for i in range(len(self.weights_vec[0])):
             temp_nur = 0
-            for j in range(0, len(inputs)-1, 1):
-                temp_nur += inputs[j] * self.weights_vec[j]
-                del self.weights_vec[0]
-            temp_nur += self.bias[j]
+            for j in range(len(inputs)):
+                temp_nur += inputs[j] * self.weights_vec[j][i]
+            temp_nur += self.bias[i]
             z_dict['z_{}'.format(i)] = self.__sigmoid(temp_nur)
-            temp_err += z_dict['z_{}'.format(i)] * self.weights_vec[-(len(inputs)-1-i)]
+            # temp_err += z_dict['z_{}'.format(i)] * self.weights_vec[-(len(inputs)-1-i)]
+
         err = inputs[-1] - self.__sigmoid(temp_err + self.bias[-1])
         err = np.linalg.norm(err)
         return err
