3d2611441eaee23c5d1a4558314d761cf29f6d47
姜韋辰 <weichen@pc217.wireless246.tku.edu.tw>
2019-12-26 10:30:47 +0800

    testing loop running backprop

--- backprop_2lay.py
+++ backprop_2lay.py
@@ -11,13 +11,13 @@ class Neuron():
     """
     inp_dict = {}
     y_dict = {}
-    delta = {}
-    update_weight_dict = defaultdict(list)
-    update_bias_dict = defaultdict(list)
     
     def __init__(self, weights_vec, bias):
         self.weights_vec = weights_vec
         self.bias = bias
+        self.delta = {}
+        self.update_weight_dict = defaultdict(list)
+        self.update_bias_dict = defaultdict(list)
 
     def forward(self, inputs):
         error = []
@@ -25,9 +25,9 @@ class Neuron():
         error_total = 0
         inputs = np.array(inputs)
         for_inp = np.copy(inputs)
+        print(for_inp)
         for h in range(len(self.weights_vec)):
             for_inp = list(self.__calculate_total_net_input(for_inp , h))
-            
             self.y_dict['h_-1y_{}'.format(h)] = inputs[h]
         print('forword y_dict: ', self.y_dict)
 
@@ -39,6 +39,8 @@ class Neuron():
 
         print('error: ', error_total, error)
         self.backward(error)
+        return self.update_weight_dict, self.update_bias_dict
+
 
     def backward(self, error):
         for h in range(len(self.weights_vec), 0, -1):
@@ -53,22 +55,19 @@ class Neuron():
         
         print('\nback update_weight:{}\nback update bias:{}'.format(self.update_weight_dict,self.update_bias_dict))
 
-        return self.update_weight_dict, self.update_bias_dict
-
-
     def update_weights(self, h, lr = 0.1):
         tmp_w_dict = defaultdict(list)
-        tmp_b_dict = defaultdict(list)
+        tmp_b_list = []
         for i in range(len(self.weights_vec[h-1])):
             for j in range(len(self.weights_vec[h-1][i])):
                 #print("delta:::",self.delta)
                 weight_element = self.weights_vec[h-1][i][j] + lr * self.delta['h_{}y_{}'.format(h, i)] * self.y_dict['h_{}y_{}'.format(h-2, j)]
                 tmp_w_dict[i].append(weight_element)
             bias_element = self.bias[h-1][i] + lr * self.delta['h_{}y_{}'.format(h, i)]
-            tmp_b_dict[i].append(bias_element)
+            tmp_b_list.append(bias_element)
 
         self.update_weight_dict['h_{}'.format(h)].append(list(tmp_w_dict.values()))
-        self.update_bias_dict['h_{}'.format(h)].append(list(tmp_b_dict.values()))
+        self.update_bias_dict['h_{}'.format(h)].append(tmp_b_list)
                 
 
     def __calculate_total_net_input(self,inp , h):
@@ -88,6 +87,7 @@ class Neuron():
         # Apply the node delta function
         if (not self.delta):
             for i in range(len(self.weights_vec[-1])):
+                # print('first node_delta:', h, i)
                 y_o = self.y_dict['h_{}y_{}'.format(len(self.weights_vec)-1, i)]
                 self.delta['h_{}y_{}'.format(h, i)] =  (-error[i]) * y_o * (1 - y_o)
         else:
@@ -105,8 +105,8 @@ class Neuron():
 class NeuronLayer():
     def __init__(self, weights_arr, bias_vec):
     # print the structure of the current neuron layer
-        self.weights_vec = weights_vec
-        self.bias = bias    
+        self.weights_vec = weights_arr
+        self.bias = bias_vec    
 
     def inspect(self):
         pass
@@ -119,11 +119,11 @@ class NeuronLayer():
     def update_weights(self, lr = 0.1):
         pass
 
-class NeuronNetwork:
+class NeuronNetwork():
     def __init__(self, weights_arrs, bias_arr):
     # weights_arrs is a 3D array, and bias_arr is a 2D array
-        self.weights_vec = weights_vec
-        self.bias = bias
+        self.weights_vec = weights_arrs
+        self.bias = bias_arr
 
     def inspect(self):
         pass
@@ -150,11 +150,11 @@ if __name__ == '__main__':
     bias_arr = [
                 [0.1,0.2,0.3],
                 [1.1,1.2,1.3,1.4],
-                [2.1,2.2,]
+                [2.1,2.2]
                ]
 
-    for i in range(100):
+    for i in range(100000):
         inp = [0.2,0.3,0.4]
-        
+        # print("go:", weights_arrs, '\n', bias_arr)
         weights_arrs, bias_arr= Neuron(weights_arrs, bias_arr).forward(inp)
-        print("go:",weights_arrs,bias_arr)
+        
