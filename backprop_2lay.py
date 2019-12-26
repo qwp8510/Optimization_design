011461b143807688934eceae69c2436479ce7290
姜韋辰 <weichen@jiangweichende-MacBook-Air.local>
2019-12-26 14:05:59 +0800

    adding structure in class neuro

--- backprop_2lay.py
+++ backprop_2lay.py
@@ -9,6 +9,7 @@ class Neuron():
     and feed-backward computations
     
     """
+
     inp_dict = {}
     y_dict = {}
     
@@ -29,7 +30,7 @@ class Neuron():
         for h in range(len(self.weights_vec)):
             for_inp = list(self.__calculate_total_net_input(for_inp , h))
             self.y_dict['h_-1y_{}'.format(h)] = inputs[h]
-        print('forword y_dict: ', self.y_dict)
+        # print('forword y_dict: ', self.y_dict)
 
         
         for i in range(len(self.weights_vec[-1])):
@@ -40,7 +41,6 @@ class Neuron():
         print('error: ', error_total, error)
         self.backward(error)
         return self.update_weight_dict, self.update_bias_dict
-
 
     def backward(self, error):
         for h in range(len(self.weights_vec), 0, -1):
@@ -90,56 +90,77 @@ class Neuron():
                 # print('first node_delta:', h, i)
                 y_o = self.y_dict['h_{}y_{}'.format(len(self.weights_vec)-1, i)]
                 self.delta['h_{}y_{}'.format(h, i)] =  (-error[i]) * y_o * (1 - y_o)
+                print("delta:::",self.delta)
         else:
             for i in range(len(self.weights_vec[h][0])):
                 tmp_delta = 0
                 for j in range(len(self.weights_vec[h])):
                     # print(h,i,j)
                     # print(self.weights_vec[h],self.weights_vec[h][j][i])
-                    tmp_delta = self.delta['h_{}y_{}'.format(h+1, j)] * self.weights_vec[h][j][i]
+                    tmp_delta += self.delta['h_{}y_{}'.format(h+1, j)] * self.weights_vec[h][j][i]
+                    # print('temp_delta:::::',tmp_delta)
                 y_o = self.y_dict['h_{}y_{}'.format(h-1, i)]
+                print("y_o::::",y_o)
                 tmp_delta = tmp_delta * y_o * (1 - y_o)
                 self.delta['h_{}y_{}'.format(h,i)] = tmp_delta
 
 
 class NeuronLayer():
     def __init__(self, weights_arr, bias_vec):
-    # print the structure of the current neuron layer
-        self.weights_vec = weights_arr
-        self.bias = bias_vec    
+        self.weights_arr = weights_arr
+        self.bias_vec = bias_vec
 
     def inspect(self):
+        # print the structure of the current neuron layer
         pass
-    def feed_forward(self, inputs):
-    # feed the node deltas of the current layer to the previous one
-        pass
+
+    def feed_forward(self, inputs): 
+        for i in range(len(self.bias_vec))
+            self.Neuron = Neuron(self.weights_vec, self.bias[i])
+            self.feed_forward(inputs)
+
     def feed_backward(self, errors):
-    # errors = layer_deltas for hidden layers
+        # feed the node deltas of the current layer to the previous one
+
+
+        # errors = layer_deltas for hidden layers
+
         pass
     def update_weights(self, lr = 0.1):
         pass
 
 class NeuronNetwork():
     def __init__(self, weights_arrs, bias_arr):
-    # weights_arrs is a 3D array, and bias_arr is a 2D array
-        self.weights_vec = weights_arrs
-        self.bias = bias_arr
+        # weights_arrs is a 3D array, and bias_arr is a 2D array
+        self.weights_arrs = weights_arrs
+        self.bias_arr = bias_arr
+
 
     def inspect(self):
-        pass
+        inputs, outputs = 0, 0
+
+        for i in range(100):
+            self.train(inputs, outputs)
 
     def feed_forward(self, inputs):
-        pass
+        self.NeuronLayer.feed_forward(inputs)
+
 
     def compute_loss(self, training_inputs, training_outputs):
-        # Uses online learning, ie updating the weights after each training epoch
+        
         pass
 
     def train(self, training_inputs, training_outputs, lr = 0.1):
-        ## private functions
-        pass
+        # Uses online learning, ie updating the weights after each training epoch
+        for i in range(len(self.bias_arr)):
+            self.NeuronLayer = NeuronLayer(weights_arrs, bias_arr)
+            self.feed_forward(training_inputs)
+
+        self.__update_weights()
+
     def __update_weights(self, lr = 0.1):
-        pass
+        # private functions
+        self.NeuronLayer.update_weights()
 
 if __name__ == '__main__':
     weights_arrs = [
@@ -153,7 +174,7 @@ if __name__ == '__main__':
                 [2.1,2.2]
                ]
 
-    for i in range(100000):
+    for i in range(10):
         inp = [0.2,0.3,0.4]
         # print("go:", weights_arrs, '\n', bias_arr)
         weights_arrs, bias_arr= Neuron(weights_arrs, bias_arr).forward(inp)
